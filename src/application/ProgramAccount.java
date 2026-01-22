758c541c1f4f908cc3bbbe2af2cd492fa70af86a
Larry Tenório <larry.lhtx@gmail.com>
2026-01-22 20:50:44 -0300

    apply switch case

--- src/application/ProgramAccount.java
+++ src/application/ProgramAccount.java
@@ -37,11 +37,57 @@ public class ProgramAccount {
 
 			account.withDraw(amount);
 
-			System.out.print(String.format("New balance: %.2f", account.getBalance()));
+			boolean running = true;
+			while (running) {
+				System.out.println("\nChoose an operation: ");
+				System.out.println("1 - Deposit");
+				System.out.println("2 - Withdraw");
+				System.out.println("3 - Check Balance");
+				System.out.println("0 - Exit");
+				int option = sc.nextInt();
 
-		} catch (DomainException e) {
-			System.out.println("error: " + e.getMessage());
-		} catch (Exception e) {
+				switch (option) {
+				case 1:
+					System.out.print("Enter amount to deposit: ");
+					double depositAmount = sc.nextDouble();
+					try {
+						account.deposit(depositAmount);
+						System.out.println(
+								"Deposit Successful! New balance: " + String.format("%.2f", account.getBalance()));
+					} catch (DomainException e) {
+						System.out.println("Error: " + e.getMessage());
+					}
+					break;
+
+				case 2:
+					System.out.print("Enter amount to withdraw: ");
+					double withdrawAmount = sc.nextDouble();
+					try {
+						account.withDraw(withdrawAmount);
+						System.out.println(
+								"Withdraw successful! New balance: " + String.format("%.2f", account.getBalance()));
+					} catch (DomainException e) {
+						System.out.println("Erro: " + e.getMessage());
+					}
+					break;
+
+				case 3:
+					System.out.println("Current balance: " + String.format("%.2f", account.getBalance()));
+					break;
+
+				case 0:
+					running = false;
+					System.out.println("Exiting. Thank you!");
+					break;
+
+				default:
+					System.out.println("Invalid option! Try again.");
+				}
+			}
+
+		}
+
+		catch (Exception e) {
 			System.out.println("Unexpected error: " + e.getMessage());
 		} finally {
 			sc.close();
